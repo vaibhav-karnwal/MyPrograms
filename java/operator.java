c5aa0a812bae801c009e0a8a2f6a1a42d93af139
vaibhav-karnwal <53268799+vaibhav-karnwal@users.noreply.github.com>
2019-11-06 18:12:22 +0530

    Add files via upload

---
+++ java/operator.java
@@ -0,0 +1,16 @@
+class operator
+{
+public static void main(String []args)
+{
+int a=1,b=4,sum=0,sub=0,div=0,multi=0;
+sum=a+b;
+sub=a-b;
+div=a/b;
+multi=a*b;
+System.out.println("addition of number"+sum);
+System.out.println("subtration of number"+sub);
+System.out.println("multiplication of number"+multi);
+System.out.println("dividion of number"+div);
+}
+}
+
