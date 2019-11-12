526d692cf096dfa33c7ca0680b1f591140acaa04
vaibhav-karnwal <53268799+vaibhav-karnwal@users.noreply.github.com>
2019-11-12 09:10:08 +0530

    Update Square_root.java

--- java/Square_root.java
+++ java/Square_root.java
@@ -7,7 +7,7 @@ public static void main(String []args)
 {
 int n;
 Scanner number=new Scanner(System.in);
-System.out.print("Enter the numbers");
+System.out.print("Enter the number");
 n=number.nextInt();
 System.out.print(Math.sqrt(n));
 }
